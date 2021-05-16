36dd716496f4e9bc4b3dea097f46d158416e9f09
Johannes Qvarford <jq.email+gitlab@pm.me>
2021-05-16 16:45:14 +0200

    Euler problem 7.

---
+++ 7-10001st-prime.rkt
@@ -0,0 +1,12 @@
+#lang racket/base
+
+ (require math/number-theory)
+
+(module+ test
+    (require rackunit)
+    (test-case "answer"
+        (define (zero-based n) (- n 1))
+        (check-equal? (nth-prime (zero-based 6)) 13)
+        (check-equal? (nth-prime (zero-based 10001)) 104743)
+    )
+)
